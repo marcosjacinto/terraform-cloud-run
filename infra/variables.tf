c215cd66302002a236950ae0b50976fd74dac942
marcos <marcos.gomesjacinto@gmail.com>
2022-10-04 18:04:08 -0300

    variables

---
+++ infra/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+    description = "Project name"
+    type = string
+}
+
+variable "region" {
+    description = "Default compute region"
+    type = string
+    default = "us-central1" 
+}
+
+variable "zone" {
+    description = "Default compute zone"
+    type = string
+    default = "us-central1-a"
+}
+
+variable "repository" {
+    description = "Artifact Registry repository's name"
+    type = string
+    default = "docker-repository"
+}
