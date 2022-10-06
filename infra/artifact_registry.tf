7f6103b10686a66e934d801fab34cbdfc505165f
marcos <marcos.gomesjacinto@gmail.com>
2022-10-06 15:41:04 -0300

    terraform: artifact registry formatting

--- infra/artifact_registry.tf
+++ infra/artifact_registry.tf
@@ -1,14 +1,14 @@
 # Create Artifact Registry Repository for Docker containers
 resource "google_artifact_registry_repository" "docker_repo" {
-  provider = google-beta
-  location = var.region
+  provider      = google-beta
+  location      = var.region
   repository_id = var.repository
-  description = "Docker repository"
-  format = "DOCKER"
+  description   = "Docker repository"
+  format        = "DOCKER"
   depends_on = [
     time_sleep.wait_30_seconds
   ]
-#   disable_on_destroy = true
+  #   disable_on_destroy = true
 }
 
 
