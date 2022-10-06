de392ab4ddf8174e8a01196e6d0deee4b22d8fe1
marcos <marcos.gomesjacinto@gmail.com>
2022-10-06 15:41:51 -0300

    terraform: service accounts and variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,22 +1,28 @@
 variable "project_id" {
-    description = "Project name"
-    type = string
+  description = "Project name"
+  type        = string
 }
 
 variable "region" {
-    description = "Default compute region"
-    type = string
-    default = "us-central1" 
+  description = "Default compute region"
+  type        = string
+  default     = "us-central1"
 }
 
 variable "zone" {
-    description = "Default compute zone"
-    type = string
-    default = "us-central1-a"
+  description = "Default compute zone"
+  type        = string
+  default     = "us-central1-a"
 }
 
 variable "repository" {
-    description = "Artifact Registry repository's name"
-    type = string
-    default = "docker-repository"
+  description = "Artifact Registry repository's name"
+  type        = string
+  default     = "docker-repository"
+}
+
+variable "docker_image" {
+  description = "The name of the Docker image"
+  type        = string
+  default     = "test-api"
 }
