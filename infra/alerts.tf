e902b7b43eb5be0b8dc3949156cd1b696a2ed1c5
marcos <marcos.gomesjacinto@gmail.com>
2022-10-23 11:48:30 -0300

    adding monitoring e-mail

--- infra/alerts.tf
+++ infra/alerts.tf
@@ -1,10 +1,19 @@
+resource "google_monitoring_notification_channel" "monitoring_email" {
+  display_name = "Monitoring E-mail"
+  type = "email"
+  labels = {
+    email_address = var.monitoring_email
+  }
+}
+
+
 resource "google_monitoring_alert_policy" "service_alert" {
   display_name = "${var.docker_image}-log-metric-alert"
   combiner = "OR"
   conditions {
-    display_name = "${var.cf_name}-errors"
+    display_name = "${var.docker_image}-errors"
     condition_matched_log {
-      filter = "resource.type=cloud_run_revision\" resource.labels.service_name=${var.docker_image}\" resource.labels.project_id=${var.project} AND severity>=WARNING"
+      filter = "resource.type=\"cloud_run_revision\" resource.labels.service_name=\"${var.docker_image}\" resource.labels.project_id=\"${var.project_id}\" AND severity>=WARNING"
     }
   }
 
@@ -14,4 +23,7 @@ resource "google_monitoring_alert_policy" "service_alert" {
       period = "3600s"
     }
   }
+
+  notification_channels = [ google_monitoring_notification_channel.monitoring_email.name ]
 }
+
